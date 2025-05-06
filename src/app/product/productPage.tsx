e867eeaf07674e4e47b79bb2e1b67883d3a9e501
erbazzafar <102461465+erbazzafar@users.noreply.github.com>
2025-05-07 01:47:38 +0500

    updates

--- src/app/product/productPage.tsx
+++ src/app/product/productPage.tsx
@@ -406,19 +406,19 @@ const ProductPage = () => {
               {gettingProduct?.brandId?.name && (
               <div>
                 <label className="text-[13px] font-medium text-gray-600">Brand:</label>
-                <span className="text-[13px] text-gray-600 ml-1">{gettingProduct?.brandId?.name || "Other "}</span>
+                <span className="text-[13px] text-gray-600 ml-1">{gettingProduct?.brandId?.name || "Other"}</span>
               </div>
               )}
               {gettingProduct?.sizeId?.name && (
               <div>
                 <label className="text-[13px] font-medium text-gray-600">Size:</label>
-                <span className="text-[13px] text-gray-600 ml-1">{gettingProduct?.sizeId?.name || "Other "}</span>
+                <span className="text-[13px] text-gray-600 ml-1">{gettingProduct?.sizeId?.name || "Other"}</span>
               </div>
               )}
               {gettingProduct?.colorId?.[0]?.name && (
               <div>
                 <label className="text-[13px] font-medium text-gray-600">Color:</label>
-                <span className="text-[13px] text-gray-600 ml-1">{gettingProduct?.colorId?.[0]?.name || "Other"}</span>
+                <span className="text-[13px] text-gray-600 ml-1">{gettingProduct?.colorId?.[0]?.name || "Other "}</span>
               </div>
               )}
 
