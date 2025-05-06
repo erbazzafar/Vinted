04e292e7602fcbd601448da7fc9e0ed42390eb48
erbazzafar <102461465+erbazzafar@users.noreply.github.com>
2025-05-07 01:14:07 +0500

    suspense added

--- src/app/sell/page.tsx
+++ src/app/sell/page.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import ProductCard from '../components/productCard'
-
+import React, { Suspense } from 'react';
+import ProductCard from '../components/productCard';
 
 function Page() {
-
   return (
     <>
-    <h1 
-      className='mt-15 max-w-4xl text-3xl font-bold mx-auto mb-2'>Sell your Item 
-    </h1>
-    <ProductCard/>
+      <h1 className='mt-15 max-w-4xl text-3xl font-bold mx-auto mb-2'>
+        Sell your Item
+      </h1>
+
+      <Suspense fallback={<p className="text-center text-gray-600">Loading product form...</p>}>
+        <ProductCard />
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default Page
+export default Page;
