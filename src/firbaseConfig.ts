9cf4e3b4b168b2ffa2372552da9bd4dc9a3828ed
erbazzafar <102461465+erbazzafar@users.noreply.github.com>
2025-05-29 00:30:32 +0500

    test: 7

--- src/firbaseConfig.ts
+++ src/firbaseConfig.ts
@@ -16,4 +16,8 @@ const firebaseConfig = {
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
+  provider.setCustomParameters({
+  prompt: "select_account"
+});
+
   export { auth, provider, signInWithRedirect };
