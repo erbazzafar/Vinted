cccd65bbf3a07cc3245e28be34403d32db80d53a
erbazzafar <102461465+erbazzafar@users.noreply.github.com>
2025-05-28 23:46:44 +0500

    fb test: 2

--- src/firbaseConfig.ts
+++ src/firbaseConfig.ts
@@ -19,24 +19,46 @@
 //   export { auth, provider, signInWithRedirect };
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDS0irfT1ol6u9PFvYaEEVi8WTOp6sopzM",
-  authDomain: "affare-doro.firebaseapp.com",
-  projectId: "affare-doro",
-  storageBucket: "affare-doro.firebasestorage.app",
-  messagingSenderId: "310039757931",
-  appId: "1:310039757931:web:79fc22ca81cb85ae155d2e",
-  measurementId: "G-C5MP9EVT9X"
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDS0irfT1ol6u9PFvYaEEVi8WTOp6sopzM",
+//   authDomain: "affare-doro.firebaseapp.com",
+//   projectId: "affare-doro",
+//   storageBucket: "affare-doro.firebasestorage.app",
+//   messagingSenderId: "310039757931",
+//   appId: "1:310039757931:web:79fc22ca81cb85ae155d2e",
+//   measurementId: "G-C5MP9EVT9X"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseSecondConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_SENDER_ID',
+  appId: 'YOUR_APP_ID',
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseSecondConfig, 'secondary'); // NOTE: name it 'secondary'
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
